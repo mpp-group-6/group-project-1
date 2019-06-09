f167b29b581285cab90db47bc2a18cff991f3903
Anthony Anyanwu <antsmc2@gmail.com>
2019-06-09 18:27:25 -0500

    slight clean up

--- src/controller/BookController.java
+++ src/controller/BookController.java
@@ -3,26 +3,20 @@
  */
 package controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import business.Author;
 import business.Book;
 import business.BookCopy;
 import dataaccess.DataAccessFacade;
 import dataaccess.dao.BookRepository;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
